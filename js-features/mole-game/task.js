a3cc94bb8bcafe3b585b4b6ba8be43c7c5150e39
Irina Agliullina <Agliullina2006@yandex.ru>
2023-05-16 17:54:44 +0400

    Выполнено задание 3 - игра Убей кротов

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,39 @@
+const winsStatistic = document.getElementById('dead');
+const lostsStatistic = document.getElementById('lost');
+let wins = 0;
+let losts = 0;
+let getHole = function( index ) {
+  return document.getElementById("hole" + index);
+}
+
+for (a = 1; a <= 9; a++) {
+  getHole(a).addEventListener('click', function(){
+    if(this.classList.contains('hole_has-mole')) {
+      wins++;
+      winsStatistic.textContent = wins;
+      } else {
+        losts++;
+        lostsStatistic.textContent = losts;
+      }
+    
+      checkGameStatus();
+  });
+}
+
+function checkGameStatus() {
+  if (wins === 10) {
+    alert('Победа!');
+    deleteStatistics();
+  }
+  if (losts === 5) {
+    alert('Вы проиграли!');
+    deleteStatistics();
+  }
+}
+
+function deleteStatistics() {
+  winsStatistic.textContent = 0;
+  wins = 0;
+  lostsStatistic.textContent = 0;
+  losts = 0;
+}
