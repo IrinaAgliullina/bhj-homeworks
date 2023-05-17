43e130603e104970ec1b9580cdaae5c65ca2ba67
Irina Agliullina <Agliullina2006@yandex.ru>
2023-05-17 10:28:05 +0400

    Исправлена формула расчета скорости кликов

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,6 +5,7 @@ let counter = 0;
 let time = new Date();
 let minutes = time.getMinutes();
 let seconds = time.getSeconds();
+let milliseconds = time.getMilliseconds();
 
 cookie.onclick = function() {
   counter++;
@@ -14,9 +15,10 @@ cookie.onclick = function() {
   let currentTime = new Date();
   let currentMinutes = currentTime.getMinutes();
   let currentSeconds = currentTime.getSeconds();
-  if (currentMinutes > minutes) {
-    clickerSpeed.textContent = 1 / (currentSeconds + ((currentMinutes - minutes) * 60 - seconds));
-  } else {
-    clickerSpeed.textContent = 1 / (currentSeconds - seconds);
-  }
+  let currentMilliseconds = currentTime.getMilliseconds();
+  
+  clickerSpeed.textContent = (1 / (currentSeconds + ((currentMinutes - minutes) * 60 - seconds) + (currentMilliseconds - milliseconds) / 1000)).toFixed(2);
+  minutes = currentMinutes;
+  seconds = currentSeconds;
+  milliseconds = currentMilliseconds;
 };
